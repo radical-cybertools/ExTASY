000d410b6c48f10a95f5fd7894060ef9eacfef3e
vivek-bala <b.vivek91@gmail.com>
2014-08-23 15:39:26 -0400

    version check

--- src/radical/ensemblemd/extasy/__init__.py
+++ src/radical/ensemblemd/extasy/__init__.py
@@ -1,3 +1,3 @@
 import os
 dir = os.path.dirname(os.path.realpath(__file__))
-version = out=open ("%s/VERSION" % dir, 'r').read().strip()
+version = out = open("%s/VERSION" % dir, 'r').read().strip()
