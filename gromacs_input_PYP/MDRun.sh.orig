7d31dd0c13bee96f1931ae3bfa516da5576b54c1
vivek-bala <vivek.balasubramanian@rutgers.edu>
2014-09-05 16:17:27 -0400

    catching upto devel

--- gromacs_input_PYP/MDRun.sh.orig
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grompp  -n index.ndx # &> grompp_${RunID}.out
-mdrun   #&> mdrun.out
-
--- gromacs_input_PYP/gromacs_run.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-if __name__ == "__main__":
-
-    os.system('/bin/bash -l -c "module load gromacs"')
-    os.system('/bin/bash -l -c "module load openmpi"')
-    os.system('/bin/bash -l -c "grompp -n index.ndx"')
-    os.system('/bin/bash -l -c "mdrun"')
