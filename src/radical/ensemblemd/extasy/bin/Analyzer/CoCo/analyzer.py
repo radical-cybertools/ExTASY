a01797d8d6c1104ddf2a13a84b9e6193e045a245
vivek-bala <b.vivek91@gmail.com>
2014-09-30 14:04:47 -0400

    reverting to mdcrd for now

--- src/radical/ensemblemd/extasy/bin/Analyzer/CoCo/analyzer.py
+++ src/radical/ensemblemd/extasy/bin/Analyzer/CoCo/analyzer.py
@@ -13,7 +13,7 @@ def Analyzer(umgr,RPconfig,Kconfig,cycle):
 
     mdtd=MDTaskDescription()
     mdtd.kernel="CoCo"
-    mdtd.arguments = ['-l','-c','python pycoco.py --grid %s --dims %s --frontpoints %s --topfile %s --mdfile *.ncdf --output pentaopt%s'%(Kconfig.grid,Kconfig.dims,Kconfig.frontpoints,os.path.basename(Kconfig.top_file),cycle)]
+    mdtd.arguments = ['-l','-c','python pycoco.py --grid %s --dims %s --frontpoints %s --topfile %s --mdfile *.mdcrd --output pentaopt%s'%(Kconfig.grid,Kconfig.dims,Kconfig.frontpoints,os.path.basename(Kconfig.top_file),cycle)]
     mdtd_bound = mdtd.bind(resource=RPconfig.REMOTE_HOST)
     cudesc = radical.pilot.ComputeUnitDescription()
     cudesc.cores = RPconfig.PILOTSIZE
@@ -22,7 +22,7 @@ def Analyzer(umgr,RPconfig,Kconfig,cycle):
     cudesc.arguments = mdtd_bound.arguments
     cudesc.input_staging = ['%s/postexec.py'%curdir,'%s/pycoco.py'%curdir,'%s'%(Kconfig.top_file)]
     for i in range(0,Kconfig.nreps):
-            cudesc.input_staging.append('md_%s_%s.ncdf'%(cycle,i))
+            cudesc.input_staging.append('md_%s_%s.mdcrd'%(cycle,i))
     cudesc.post_exec = ['python postexec.py %s %s' % (Kconfig.nreps,cycle)]
     cudesc.mpi = True
     cudesc.output_staging = []
