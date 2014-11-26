868c48bd2e7f2438a9ce722d8698be805a70b24a
vivek-bala <b.vivek91@gmail.com>
2014-11-26 08:46:54 -0500

    num iteration fix

--- src/radical/ensemblemd/extasy/bin/runme.py
+++ src/radical/ensemblemd/extasy/bin/runme.py
@@ -147,7 +147,7 @@ def main():
                     for line in f:
                         paths.append(line.strip())
 
-        for i in range(Kconfig.start_iter,Kconfig.num_iterations):
+        for i in range(Kconfig.start_iter,Kconfig.start_iter + Kconfig.num_iterations):
             paths=paths + (Preprocessing(Kconfig, umgr, i,paths))
             if Kconfig.simulator:
                 Simulator(umgr, RPconfig, Kconfig, i,paths)
