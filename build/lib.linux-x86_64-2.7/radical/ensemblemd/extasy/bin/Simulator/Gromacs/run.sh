8476254842795aa4d9d47c3e6702d12b6b51f10c
vivek-bala <b.vivek91@gmail.com>
2015-02-26 06:38:13 -0500

    version bump in another file

---
+++ build/lib.linux-x86_64-2.7/radical/ensemblemd/extasy/bin/Simulator/Gromacs/run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+mdpfile_name=$1
+grofile_name=$2
+topfile_name=$3
+output_grofile_name=$4
+N=$5
+tprfile_name=topol.tpr
+trrfile_name=traj.trr
+edrfile_name=ener.edr
+
+if [ -n "$grompp_options" ]; then
+    grompp_options=$grompp_options
+else
+    grompp_options=''
+fi
+
+if [ -n "$mdrun_options" ]; then
+    mdrun_options=$mdrun_options
+else
+    mdrun_options=''
+fi
+
+if [ -n "$ndxfile" ]; then
+    ndxfile_option="-n "$ndxfile
+else
+    ndxfile_option=''
+fi
+
+startgro=$grofile_name
+tmpstartgro=tmpstart.gro
+outgro=$output_grofile_name
+
+natoms=$(sed -n '2p' $startgro)
+nlines_per_frame=$((natoms+3))
+
+#nlines=`wc -l %(directory_name)s/$startgro| cut -d' ' -f1`
+nlines=`wc -l $startgro| cut -d' ' -f1`
+nframes=$((nlines/nlines_per_frame))
+
+rm -rf $outgro
+
+for idx in `seq 1 $nframes`; do
+
+  start=$(($nlines_per_frame*(idx-1)+1))
+  end=$(($nlines_per_frame*idx))
+  sed "$start"','"$end"'!d' $startgro > $tmpstartgro
+
+  # gromacs preprocessing & MD
+  grompp $grompp_options $ndxfile_option -f $mdpfile_name -c $tmpstartgro -p $topfile_name -o $tprfile_name
+  mdrun -nt $N $mdrun_options -s $tprfile_name -o $trrfile_name -e $edrfile_name
+
+  # store data
+  cat confout.gro >> $outgro
+
+done
+
---
+++ build/lib.linux-x86_64-2.7/radical/ensemblemd/extasy/bin/Simulator/Gromacs/simulator.py
@@ -0,0 +1,117 @@
+__author__ = 'vivek'
+
+import radical.pilot
+from radical.ensemblemd.mdkernels import MDTaskDescription
+import os
+import time
+import glob
+
+
+def Simulator(umgr,RPconfig,Kconfig,cycle):
+
+    curdir = os.path.dirname(os.path.realpath(__file__))
+
+    MY_STAGING_AREA = 'staging:///'
+
+    grompp_name = os.path.basename(Kconfig.mdp_file)
+    topol_name = os.path.basename(Kconfig.top_file)
+
+    nsave = Kconfig.nsave
+
+    print 'Starting Simulation'
+
+    gromacs_tasks = []
+    for i in range(0, Kconfig.num_CUs):
+        mdtd = MDTaskDescription()
+        mdtd.kernel = "GROMACS"
+        if Kconfig.grompp_options is not None:
+            grompp_opts = Kconfig.grompp_options
+        else:
+            grompp_opts = ''
+        if Kconfig.mdrun_options is not None:
+            mdrun_opts = Kconfig.mdrun_options
+        else:
+            mdrun_opts = ''
+        if Kconfig.ndx_file is not None:
+            ndxfile_name = os.path.basename(Kconfig.ndx_file)
+        else:
+            ndxfile_name = ''
+        mdtd.arguments = ['run.py', '--mdp', '%s' % grompp_name, '--gro', 'start.gro', '--top', '%s' % topol_name, '--out', 'out.gro']
+        mdtd_bound = mdtd.bind(resource=RPconfig.REMOTE_HOST)
+        gromacs_task = radical.pilot.ComputeUnitDescription()
+        gromacs_task.pre_exec = ['export grompp_options="%s" mdrun_options="%s" ndxfile="%s"' % (grompp_opts, mdrun_opts, ndxfile_name)] + mdtd_bound.pre_exec
+        gromacs_task.executable = 'python'
+        gromacs_task.arguments = mdtd_bound.arguments
+        gromacs_task.mpi = True
+        gromacs_task.cores = Kconfig.num_cores_per_sim_cu
+
+        run_stage = {
+                        'source': MY_STAGING_AREA + 'run.py',
+                        'target': 'run.py',
+                        'action': radical.pilot.LINK
+                    }
+
+        gro_stage = {
+                        'source': MY_STAGING_AREA + 'iter{0}/start{1}.gro'.format(cycle, i),
+                        'target': 'start.gro',
+                        'action': radical.pilot.LINK
+                    }
+
+        grompp_stage = {
+                        'source': MY_STAGING_AREA + grompp_name,
+                        'target': grompp_name,
+                        'action': radical.pilot.LINK
+                    }
+
+        topol_stage = {
+                        'source': MY_STAGING_AREA + topol_name,
+                        'target': topol_name,
+                        'action': radical.pilot.LINK
+                    }
+
+        gromacs_task.input_staging = [run_stage, gro_stage, grompp_stage, topol_stage]
+
+        if Kconfig.ndx_file is not None:
+            ndx_stage = {
+                            'source': MY_STAGING_AREA + ndxfile_name,
+                            'target': ndxfile_name,
+                            'action': radical.pilot.LINK
+                        }
+            gromacs_task.input_staging.append(ndx_stage)
+
+        if Kconfig.itp_file_loc is not None:
+            for itpfile in glob.glob(Kconfig.itp_file_loc + '*.itp'):
+                temp = {
+                            'source': MY_STAGING_AREA + itpfile,
+                            'target': itpfile,
+                            'action': radical.pilot.LINK
+                        }
+                gromacs_task.input_staging.append(temp)
+
+        out_stage = {
+                        'source': 'out.gro',
+                        'target': MY_STAGING_AREA + 'iter{0}/out{1}.gro'.format(cycle, i),
+                        'action':radical.pilot.LINK
+                    }
+
+        gromacs_task.output_staging = [out_stage]
+
+        gromacs_tasks.append(gromacs_task)
+
+    units = umgr.submit_units(gromacs_tasks)
+
+    start_times = []
+    end_times = []
+
+    # Wait for all compute units to finish.
+    umgr.wait_units()
+
+    try:
+        for unit in units:
+            start_times.append(unit.start_time)
+            end_times.append(unit.stop_time)
+    
+        print 'Simulation Execution Time : ', (max(end_times)-min(start_times)).total_seconds()
+
+    except:
+        pass
