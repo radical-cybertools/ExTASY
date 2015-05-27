4efa0d06f751c6b77d2ff71e85027b3422b2001f
vivek-bala <b.vivek91@gmail.com>
2015-05-27 16:09:14 -0400

    ver fix in setup

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ setup_args = {
     'install_requires' : ['setuptools>=1',
                           'radical.pilot==0.33',
                           'argparse',
-                          'radical.ensemblemd.mdkernels==0.1'
+                          'radical.ensemblemd.mdkernels'
                          ],
     'tests_require' : ['setuptools>=1','nose','radical.pilot==0.33','radical.ensemblemd.mdkernels==0.1'],
     'test_suite' : 'radical.ensemblemd.extasy.tests',
