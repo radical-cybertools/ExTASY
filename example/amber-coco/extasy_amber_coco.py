ca01c7d85665ad34fed3d888f8b14ceaab161b92
vivek-bala <b.vivek91@gmail.com>
2015-07-17 17:45:32 -0400

    update examples

--- example/amber-coco/extasy_amber_coco.py
+++ example/amber-coco/extasy_amber_coco.py
@@ -155,22 +155,39 @@ class Extasy_CocoAmber_Static(SimulationAnalysisLoop):
                             --topfile   = Topology filename
                             --cycle     = current iteration number
         '''
-        k = Kernel(name="md.amber")
-        k.arguments = ["--mininfile={0}".format(os.path.basename(Kconfig.minimization_input_file)),
-                       "--mdinfile={0}".format(os.path.basename(Kconfig.md_input_file)),
+        k1 = Kernel(name="md.amber")
+        k1.arguments = ["--mininfile={0}".format(os.path.basename(Kconfig.minimization_input_file)),
+                       #"--mdinfile={0}".format(os.path.basename(Kconfig.md_input_file)),
                        "--topfile={0}".format(os.path.basename(Kconfig.top_file)),
+                       "--crdfile={0}".format(os.path.basename(Kconfig.initial_crd_file)),
                        "--cycle=%s"%(iteration)]
-        k.link_input_data = ['$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.minimization_input_file)),
+        k1.link_input_data = ['$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.minimization_input_file)),
                              '$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.top_file)),
-                             '$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.md_input_file))]
-        k.copy_input_data = 
+                             '$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.initial_crd_file))]
+        k1.cores=2
         if((iteration-1)==0):
-            k.link_input_data = k.link_input_data + ['$PRE_LOOP/{0} > min1.crd'.format(os.path.basename(Kconfig.initial_crd_file))]
+            k1.link_input_data = k1.link_input_data + ['$PRE_LOOP/{0} > min1.crd'.format(os.path.basename(Kconfig.initial_crd_file))]
         else:
-            k.link_input_data = k.link_input_data + ['$PREV_ANALYSIS_INSTANCE_1/min{0}{1}.crd > min{2}.crd'.format(iteration-1,instance-1,iteration)]
+            k1.link_input_data = k1.link_input_data + ['$PREV_ANALYSIS_INSTANCE_1/min{0}{1}.crd > min{2}.crd'.format(iteration-1,instance-1,iteration)]
+        k1.copy_output_data = ['md{0}.crd > $PRE_LOOP/md_{0}_{1}.crd'.format(iteration,instance)]
+        
+
+        k2 = Kernel(name="md.amber")
+        k2.arguments = [
+                            "--mdinfile={0}".format(os.path.basename(Kconfig.md_input_file)),
+                            "--topfile={0}".format(os.path.basename(Kconfig.top_file)),
+                            "--cycle=%s"%(iteration)
+                        ]
+        k2.link_input_data = [  
+                                "$PRE_LOOP/{0}".format(os.path.basename(Kconfig.md_input_file)),
+                                "$PRE_LOOP/{0}".format(os.path.basename(Kconfig.top_file)),
+                                "$PRE_LOOP/md_{0}_{1}.crd > md{0}.crd".format(iteration,instance),
+                            ]
         if(iteration%Kconfig.nsave==0):
-            k.download_output_data = ['md{0}.ncdf > backup/iter{0}/md_{0}_{1}.ncdf'.format(iteration,instance)]
-        return k
+            k1.download_output_data = ['md{0}.ncdf > backup/iter{0}/md_{0}_{1}.ncdf'.format(iteration,instance)]
+
+        k2.cores = 2
+        return [k1,k2]
 
 
     def analysis_step(self, iteration, instance):
@@ -190,21 +207,34 @@ class Extasy_CocoAmber_Static(SimulationAnalysisLoop):
                             --output        = Output filename
                             --cycle         = Current iteration number
         '''
-        k = Kernel(name="md.coco")
-        k.arguments = ["--grid={0}".format(Kconfig.grid),
+        k1 = Kernel(name="md.coco")
+        k1.arguments = ["--grid={0}".format(Kconfig.grid),
                        "--dims={0}".format(Kconfig.dims),
                        "--frontpoints={0}".format(Kconfig.num_CUs),
                        "--topfile={0}".format(os.path.basename(Kconfig.top_file)),
                        "--mdfile=*.ncdf",
-                       "--output=pentaopt%s"%(iteration),
-                       "--cycle=%s"%(iteration)]
-        k.link_input_data = ['$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.top_file)),
-                             '$PRE_LOOP/postexec.py']
-        k.cores = RPconfig.PILOTSIZE
+                       "--output=pentaopt%s"%(iteration)]
+        k1.cores = RPconfig.PILOTSIZE
+
+        k1.link_input_data = ['$PRE_LOOP/{0}'.format(os.path.basename(Kconfig.top_file))]
         for iter in range(1,iteration+1):
             for i in range(1,Kconfig.num_CUs+1):
-                k.link_input_data = k.link_input_data + ['$SIMULATION_ITERATION_{0}_INSTANCE_{1}/md{0}.ncdf > md_{0}_{1}.ncdf'.format(iter,i)]
-        return k
+                k1.link_input_data = k1.link_input_data + ['$SIMULATION_ITERATION_{0}_INSTANCE_{1}/md{0}.ncdf > md_{0}_{1}.ncdf'.format(iter,i)]
+
+        k1.copy_output_data = ['STDERR > $PRE_LOOP/STDERR']
+        for i in range(0,Kconfig.num_CUs):
+            k1.copy_output_data = k1.copy_output_data + ['pentaopt{0}{1}.pdb > $PRE_LOOP/pentaopt{0}{1}.pdb'.format(iteration,i)]
+
+
+        k2 = Kernel(name="md.tleap")
+        k2.arguments = ["--numofsims={0}".format(Kconfig.num_CUs),
+                        "--cycle={0}".format(iteration)]
+
+        k2.link_input_data = []
+        for i in range(0,Kconfig.num_CUs):
+            k2.link_input_data = k2.link_input_data + ['$PRE_LOOP/pentaopt{0}{1}.pdb > pentaopt{0}{1}.pdb'.format(iteration,i)]
+
+        return [k1,k2]
 
     def post_loop(self):
         pass
